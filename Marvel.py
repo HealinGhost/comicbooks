8e2145a7b29fcbde46ef6f6ab823eb5655c7e527
HealinGhost <71658197+HealinGhost@users.noreply.github.com>
2022-03-10 21:51:09 +0100

    created the tables

--- Marvel.py
+++ Marvel.py
@@ -16,58 +16,101 @@ def create_database(cursor, DB_NAME):
     return
 
 
-# Function that creaes a table
-def create_table(cursor, data, table_name):
+# Function that creates all the tables
+def create_tables(cursor):
 
-    creat_comics = "CREATE TABLE `cars` (" \
-                 "  `id` int(11) NOT NULL," \
-                 "  `name` varchar(64) NOT NULL," \
-                 "  `year` int(4) NOT NULL," \
-                 "  `artist` varchar(64) NOT NULL," \
-                 "  `writer` SMALLINT NOT NULL," \
-                 "  PRIMARY KEY (`id`)" \
-                 ") ENGINE=InnoDB"
+    tables = []
 
+    create_comics = "CREATE TABLE `comic_books` (" \
+                    "  `id` int(11) NOT NULL," \
+                    "  `name` varchar(64) NOT NULL," \
+                    "  `year` int(5) NOT NULL," \
+                    "  `artist` varchar(64) NOT NULL," \
+                    "  `writer` varchar(64) NOT NULL," \
+                    "  PRIMARY KEY (`id`)" \
+                    ") ENGINE=InnoDB"
 
-        cursor.execute(new_table)
-    except mysql.connector.Error as err:
-        if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
-            print("already exists.")
+    tables.append(create_comics)
+
+    create_movies = "CREATE TABLE `movies` (" \
+                    "  `id` int(11) NOT NULL," \
+                    "  `title` varchar(64) NOT NULL," \
+                    "  `director` varchar(64) NOT NULL," \
+                    "  `release_year` int(5) NOT NULL," \
+                    "  `comic_id` int(11) NULL," \
+                    "  PRIMARY KEY (`id`)" \
+                    ") ENGINE=InnoDB"
+
+    tables.append(create_movies)
+
+    create_participants = "CREATE TABLE `participants` (" \
+                          "  `comic_id` int(11) NOT NULL," \
+                          "  `character_id` int(11) NOT NULL" \
+                          ") ENGINE=InnoDB"
+
+    tables.append(create_participants)
+
+    create_characters = "CREATE TABLE `characters` (" \
+                        "  `id` int(11) NOT NULL," \
+                        "  `name` varchar(64) NOT NULL," \
+                        "  `side` varchar(64) NOT NULL," \
+                        "  `super_power` varchar(255) NOT NULL," \
+                        "  `release_year` int(5) NOT NULL," \
+                        "  PRIMARY KEY (`id`)" \
+                        ") ENGINE=InnoDB"
+
+    tables.append(create_characters)
+
+    # loops through all the queries
+    for table in tables:
+
+        try:
+            cursor.execute(table)
+        except mysql.connector.Error as err:
+            if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+                print("already exists.")
+            else:
+                print(err.msg)
         else:
-            print(err.msg)
-    else:
-        print("OK")
+            print("OK")
+
     return
 
 
 # Reading csv and preparing them for sql
-def readfile(list, path):
-    with open(path, "r", encoding='utf-8') as csvreader:
-        csvfile = csv.reader(csvreader, delimiter=",")
-        temp_list = []
-        for row in csvfile:
-            temp_list.append(row)
-            new_string = "VALUES ("
-            for i in range(len(row) - 1):
-                new_string += "'" + row[i].replace("'", "''") + "', "
-
-            new_string += "'" + row[len(row) - 1].strip("\n") + "');"
-            list.append(new_string)
-        # I set the first row to be the data of each columns
-        list[0] = temp_list[0]
-        csvreader.close()
+def readfile(cursor, cnx, path, table_name):
+    with open(path, "r") as csvfile:
+        csvdata = csv.reader(csvfile, delimiter=",")
+        line_count = 0
+        for line in csvdata:
+            if line_count == 0:
+                line_count += 1
+            else:
+                new_list = []
+                for value in line:
+                    if value != "NA":
+                        new_list.append(value)
+                values = len(new_list)
+                temp_string = " VALUES ("
+                for i in range(values - 1):
+                    temp_string += "%s,"
+                temp_string += "%s)"
+                stor = tuple(new_list)
+                sql = "INSERT INTO " + table_name + temp_string
+                cursor.execute(sql, stor)
+                cnx.commit()
     return
 
 
 # Function that inserts the values in the given table
 def insert_into(cursor, cnx, data, table_name):
-
     # Creates the insert line
     temp_list = data
     insert = "INSERT INTO " + table_name + " ("
     for i in range(len(data[0]) - 1):
         insert += data[0][i] + ", "
     insert += data[0][-1] + ")"
+    print(insert)
 
     # remove the first value, cause it is not used
     del temp_list[0]
@@ -109,11 +152,11 @@ def main():
             print("Database {} created succesfully.".format(DB_NAME))
             cnx.database = DB_NAME
 
-        # Creating a table of planets in db
-        # data = []
-        # readfile(data, "planets.csv")
-        create_table(cursor, data, "planets")
-        insert_into(cursor, cnx, data, "planets")
+    # create_tables(cursor)
+
+    data = []
+    readfile(cursor, cnx, "MarvelParticipants.csv", "participants")
+    readfile(cursor, cnx, "MarvelMovies.csv", "movies")
 
     cnx.close()
     cursor.close()
